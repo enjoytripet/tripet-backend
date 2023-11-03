97a15e290b53b23a16e7a020effc5486dc763141
dongsuu <suudh99@naver.com>
2023-11-03 15:32:16 +0900

    init

---
+++ src/main/java/com/enjoy/tripet/TripetApplication.java
@@ -0,0 +1,13 @@
+package com.enjoy.tripet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enjoy/tripet/TripetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enjoy.tripet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
